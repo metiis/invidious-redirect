90e7ce6cbde484ac8e04f81f2b2f5175f621aa3d
Julius <git@metiis.com>
2018-07-16 16:18:59 +0200

    Changed hooktube to invidious. See README.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # hooktube-redirect
-A (very) small WebExtension to redirect YouTube links to HookTube.
 
-I am not affiliated with YouTube or HookTube.
+# Now redirects to invidio.us instead of hooktube - see Hooktube changelog for details
+
+A (very) small WebExtension to redirect YouTube links to Invidious.
+
+I am not affiliated with YouTube or Invidious.
 
 This work is released into the public domain. See UNLICENSE for details.
--- makehook.js
+++ makehook.js
@@ -1,10 +1,10 @@
 var loc = window.location.href;
 if (loc.includes("youtube")){
-	window.location=loc.replace("youtube", "hooktube");
+	window.location=loc.replace("youtube.com", "invidio.us");
 }
 else if (loc.includes("youtu.be")){
-	window.location=loc.replace("youtu.be/", "hooktube.com/watch?v=");
+	window.location=loc.replace("youtu.be/", "invidio.us/watch?v=");
 }
 else if (loc.includes("youtube-nocookie")){
-	window.location=loc.replace("youtube-nocookie.com/", "hooktube.com/");
+	window.location=loc.replace("youtube-nocookie.com/", "invidio.us/");
 }
