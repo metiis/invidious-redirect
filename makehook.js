866032566f6bfbed91db62ff8275ed3479820177
Metiis <mail@metiis.com>
2018-07-25 23:29:06 +0200

    now actually works

--- makehook.js
+++ makehook.js
@@ -1,16 +1,22 @@
 var loc = window.location.href;
-if(loc.includes("m.youtube.com")){
-        window.location=loc.replace("m.youtube.com", "invidio.us");
+var newloc = loc;
+if(newloc.includes("m.youtube.com")){
+        newloc=newloc.replace("m.youtube.com", "invidio.us");
 }
-else if (loc.includes("youtube")){
-	window.location=loc.replace("youtube.com", "invidio.us");
+else if (newloc.includes("youtube")){
+	newloc=newloc.replace("youtube.com", "invidio.us");
 }
-else if (loc.includes("youtu.be")){
-	window.location=loc.replace("youtu.be/", "invidio.us/watch?v=");
+else if (newloc.includes("youtu.be")){
+	newloc=newloc.replace("youtu.be/", "invidio.us/watch?v=");
 }
-else if (loc.includes("youtube-nocookie")){
-	window.location=loc.replace("youtube-nocookie.com/", "invidio.us/");
+else if (newloc.includes("youtube-nocookie")){
+	newloc=newloc.replace("youtube-nocookie.com/", "invidio.us/");
 }
-if(loc.includes("/results?search_query")){
-        window.location=loc.replace("/results?search_query", "/search?q");
+if(newloc.includes("/results?q")){
+        newloc=newloc.replace("/results?q", "/search?q");
 }
+if(newloc.includes("/results?search_query")){
+        newloc=newloc.replace("/results?search_query", "/search?q");
+}
+window.location=newloc;
+
