376900bacd7a74c8c0269d6978e4536fb3ad1081
Metiis <mail@metiis.com>
2018-07-25 22:56:10 +0200

    Fixed mobile website. Fixes #4.

--- makehook.js
+++ makehook.js
@@ -1,5 +1,8 @@
 var loc = window.location.href;
-if (loc.includes("youtube")){
+if(loc.includes("m.youtube.com")){
+        window.location=loc.replace("m.youtube.com", "invidio.us");
+}
+else if (loc.includes("youtube")){
 	window.location=loc.replace("youtube.com", "invidio.us");
 }
 else if (loc.includes("youtu.be")){
@@ -7,4 +10,7 @@ else if (loc.includes("youtu.be")){
 }
 else if (loc.includes("youtube-nocookie")){
 	window.location=loc.replace("youtube-nocookie.com/", "invidio.us/");
+}
+if(loc.includes("/results?search_query")){
+        window.location=loc.replace("/results?search_query", "/search?q");
 }
