d67c3d3eedbca520ea02c1446fade1d50c2e6fee
Chenkai Luo <40770208+Chenkail@users.noreply.github.com>
2020-08-02 00:05:28 +0000

    Change to snopyta instance
    Switching to snopyta instance as the main invidio.us instance is shutting down on 2020-09-01:
    https://omar.yt/posts/stepping-away-from-open-source

--- makehook.js
+++ makehook.js
@@ -1,16 +1,16 @@
 var loc = window.location.href;
 var newloc = loc;
 if(newloc.includes("m.youtube.com")){
-        newloc=newloc.replace("m.youtube.com", "invidio.us");
+        newloc=newloc.replace("m.youtube.com", "invidious.snopyta.org");
 }
 else if (newloc.includes("youtube")){
-	newloc=newloc.replace("youtube.com", "invidio.us");
+	newloc=newloc.replace("youtube.com", "invidious.snopyta.org");
 }
 else if (newloc.includes("youtu.be")){
-	newloc=newloc.replace("youtu.be/", "invidio.us/watch?v=");
+	newloc=newloc.replace("youtu.be/", "invidious.snopyta.org/watch?v=");
 }
 else if (newloc.includes("youtube-nocookie")){
-	newloc=newloc.replace("youtube-nocookie.com/", "invidio.us/");
+	newloc=newloc.replace("youtube-nocookie.com/", "invidious.snopyta.org/");
 }
 if(newloc.includes("/results?q")){
         newloc=newloc.replace("/results?q", "/search?q");
