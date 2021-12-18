c2edb1be69197ce0589186c2fd88cf3c7a045c53
kmille <github@androidloves.me>
2021-12-18 20:52:18 +0100

    Switch to yewtu.be

--- makehook.js
+++ makehook.js
@@ -1,16 +1,16 @@
 var loc = window.location.href;
 var newloc = loc;
 if(newloc.includes("m.youtube.com")){
-        newloc=newloc.replace("m.youtube.com", "invidio.us");
+        newloc=newloc.replace("m.youtube.com", "yewtu.be");
 }
 else if (newloc.includes("youtube")){
-	newloc=newloc.replace("youtube.com", "invidio.us");
+	newloc=newloc.replace("youtube.com", "yewtu.be");
 }
 else if (newloc.includes("youtu.be")){
-	newloc=newloc.replace("youtu.be/", "invidio.us/watch?v=");
+	newloc=newloc.replace("youtu.be/", "yewtu.be/watch?v=");
 }
 else if (newloc.includes("youtube-nocookie")){
-	newloc=newloc.replace("youtube-nocookie.com/", "invidio.us/");
+	newloc=newloc.replace("youtube-nocookie.com/", "yewtu.be/");
 }
 if(newloc.includes("/results?q")){
         newloc=newloc.replace("/results?q", "/search?q");
